3569428aad4e5a2b33ed333d5f7260fdf53533c4
Karl Manalo <karlmanalo@comcast.net>
2020-05-18 12:52:35 -0600

    Integrate iwth dev database

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 TODO: clone the repo
 
+## Setup
+
+'''sh
+# mac version:
+FLASK_APP=web_app flask run
+
+# windows version:
+set FLASK_APP=web_app 
+flask run
+
 ## Usage:
 '''sh
 # mac version:
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,11 +2,21 @@
 
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
 def create_app():
     app = Flask(__name__)
+
+    DATABASE_URI = "sqlite:///C:\\Users\\Karl\\Documents\\lambda\\u3s3\\twitoff\\twitoff_development.db" # using relative filepath
+#DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
+#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
