d2723053367a2b10e1d1c46fe6f7ed61e3a9c2ba
Karl Manalo <karlmanalo@comcast.net>
2020-05-19 19:50:44 -0600

    Turns in U3S3M2 Assignment

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,13 +5,15 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
+
 
 def create_app():
     app = Flask(__name__)
 
     DATABASE_URI = "sqlite:///C:\\Users\\Karl\\Documents\\lambda\\u3s3\\twitoff\\twitoff_development.db" # using relative filepath
-#DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
-#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+    #DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
+    #DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     db.init_app(app)
@@ -19,6 +21,7 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(twitter_routes)
     return app
 
 if __name__ == "__main__":
