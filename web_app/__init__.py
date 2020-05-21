07ba486745ec3dfb8c648364a51b5a85d0c30f80
Karl Manalo <karlmanalo@comcast.net>
2020-05-21 11:14:30 -0600

    Fix books error

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,38 +1,8 @@
-# # web_app/__init__.py
-
-# from flask import Flask
-
-# from web_app.models import db, migrate
-# from web_app.routes.home_routes import home_routes
-# from web_app.routes.book_routes import book_routes
-# from web_app.routes.twitter_routes import twitter_routes
-# from web_app.routes.stats_routes import stats_routes
-
-# DATABASE_URI = "sqlite:////Users/mjr/Desktop/twitoff-14/twitoff_development_14.db" # using absolute filepath on Mac (recommended)
-
-# # SECRET_KEY = "super secret" # todo: use env var to customize
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.config["SECRET_KEY"] = SECRET_KEY
-
-#     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#     db.init_app(app)
-#     migrate.init_app(app, db)
-
-#     app.register_blueprint(home_routes)
-#     app.register_blueprint(book_routes)
-#     app.register_blueprint(twitter_routes)
-#     app.register_blueprint(stats_routes)
-#     return app
-
-# if __name__ == "__main__":
-#     my_app = create_app()
-#     my_app.run(debug=True)
 # web_app/__init__.py
 
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
@@ -40,13 +10,15 @@ from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
+load_dotenv()
+
+DATABASE_URL =  os.getenv("DATABASE_URL")
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
-
-    DATABASE_URI = "sqlite:///C:\\Users\\Karl\\Documents\\lambda\\u3s3\\twitoff\\twitoff_development.db" # using relative filepath
-    #DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
-    #DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+    app.config["SECRET_KEY"] = SECRET_KEY
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
